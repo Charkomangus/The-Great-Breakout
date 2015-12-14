ac0d6cc778426499f0f28a1147fa3b80d0f1d4eb
Charkomangus <CharKoundourakis@gmail.com>
2015-12-14 11:17:40 +0000

    First upload of project

---
+++ TheGreatEscape/TheGreatEscape/GameCode/GraphicType.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using TheGreatEscape.GameCode;
+
+namespace PlatformGame
+{
+    /// <summary>
+    /// The simplest Graphic type, it consists of a Texture and a Vector 2 Position.
+    /// </summary>
+    internal class StaticGraphic
+    {
+        protected Vector2 m_position;
+        protected Texture2D m_txr;
+        public StaticGraphic(Vector2 Position, Texture2D txrImage)
+        {
+            m_position = Position;
+            m_txr = txrImage;
+        }
+        public virtual void Draw(SpriteBatch sBatch)
+        {
+            sBatch.Draw(m_txr, m_position, Color.White);
+        }
+    }
+    /// <summary>
+    /// A static Graphi with velocity. It can move now.
+    /// </summary>
+    internal class MotionGraphic : StaticGraphic
+    {
+        protected Vector2 m_velocity;
+        public  MotionGraphic(Vector2 Position, Texture2D txr) : base(Position, txr)
+        {
+           m_velocity = Vector2.Zero;
+        }
+    }
+    /// <summary>
+    /// A motion Graphic with a defined Bounding Sphere
+    /// </summary>
+    internal class Circle : MotionGraphic
+    {
+        protected BoundingSphere m_sphere;
+        public Circle(Vector2 Position, Texture2D txr) : base(Position, txr)
+        {
+        }
+        public void Update(GameTime gt, Vector2 m_position)
+        {
+            m_sphere = UtilityFunctions.CreateSphere(m_position, m_txr);
+        }
+        public BoundingSphere GetSphere()
+        {
+            return UtilityFunctions.CreateSphere(m_position, m_txr);
+        }
+        public virtual void Draw(SpriteBatch sBatch, Texture2D circleTxr)
+        {
+            sBatch.Draw(m_txr, m_position, Color.White);
+        }
+    }
+    /// <summary>
+    /// A motion Graphic with a defined Rectangle and BoundingBox
+    /// </summary>
+    internal class Square : MotionGraphic
+    {
+        protected Rectangle m_Rect;
+        protected BoundingBox m_Box ;
+        public Square(Vector2 Position, Texture2D txr): base(Position, txr)
+        {
+        }
+        public virtual void Update(GameTime gt)
+        {
+            m_Rect = GetBounds();
+            m_Box = GetBoundingBox();
+        }
+        /// <summary>
+        /// Return a Bounding box the size of the brick and in the position of the brick
+        /// </summary>
+        /// <returns></returns>
+        public Rectangle GetBounds()
+        {
+            return new Rectangle((int)m_position.X, (int)m_position.Y, m_txr.Width, m_txr.Height);
+        }
+        /// <summary>
+        /// Return a Bounding box the size of the brick and in the position of the brick
+        /// </summary>
+        /// <returns></returns>
+        public BoundingBox GetBoundingBox()
+        {
+            return UtilityFunctions.ConvertRectangleToBB(GetBounds());
+        }
+        public virtual void Draw(SpriteBatch sBatch, Texture2D box_min, Texture2D box_max)
+        {
+            sBatch.Draw(m_txr, m_position, Color.White);
+        }
+    }
+}
---
+++ TheGreatEscape/TheGreatEscape/GameCode/UtilityFunctions.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace TheGreatEscape.GameCode
+{
+    /// <summary>
+    /// A repository of useful functions that remain spearate from the main project
+    /// </summary>
+    public static class UtilityFunctions
+    {
+        /// <summary>
+        /// Quickly convers a Rectangle to a Bounding Box
+        /// </summary>
+        /// <param name="rectangle"></param>An objects rectangle
+        /// <returns></returns>
+        public static BoundingBox ConvertRectangleToBB(Rectangle rectangle)
+        {
+            Vector3 min = new Vector3(rectangle.X, rectangle.Y, 0);
+            Vector3 max = new Vector3(rectangle.X + rectangle.Width, rectangle.Y + rectangle.Height, 0);
+            return new BoundingBox(min, max);
+        }
+        /// <summary>
+        ///  Quickly convers a Bounding Box to a Rectangle
+        /// </summary>
+        /// <param name="box"></param>An objects BB
+        /// <returns></returns>
+        public static Rectangle ConvertBBToRectangle(BoundingBox box)
+        {
+            Rectangle rect = new Rectangle();
+            rect.X = (int) box.Min.X;
+            rect.Y = (int) box.Min.Y;
+            rect.Width = (int) (Math.Abs(box.Min.X - box.Max.X));
+            rect.Height = (int) (Math.Abs(box.Min.Y - box.Max.Y));
+            return rect;
+        }
+        /// <summary>
+        /// Using an objects position and texture create a sphere collider around
+        /// </summary>
+        /// <param name="position"></param>
+        /// <param name="txr"></param>
+        /// <returns></returns>
+        public static BoundingSphere CreateSphere(Vector2 position, Texture2D txr)
+        {
+            return new BoundingSphere(new Vector3(position, 0), txr.Width/2f);
+        }
+        /// <summary>
+        /// Convert a Vector3 into a Vector2
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public static Vector2 V3ToV2(Vector3 input)
+        {
+            return new Vector2(input.X, input.Y);
+        }
+    }
+}
